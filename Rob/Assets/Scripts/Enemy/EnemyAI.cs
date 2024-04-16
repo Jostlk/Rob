044489c54bd3b8d9650e517897500a95a9ba919e
Jostlk <evgenijlopyrev29@gmail.com>
2024-04-16 21:21:29 +0300

    AI
    Добавил ИИ

---
+++ Rob/Assets/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public List<Transform> PatrolPoints;
+    private NavMeshAgent _navMeshAgent;
+    public PlayerController Player;
+    private bool _isPlayerNoticed;
+    public float ViewAngle;
+
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        PickNewPatrolPoint();
+    }
+
+    void Update()
+    {
+        if (!_isPlayerNoticed)
+        {
+            if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+            {
+                PickNewPatrolPoint();
+            }
+        }
+        NoticePlayerUpdate();
+        if (_isPlayerNoticed)
+        {
+            _navMeshAgent.destination = Player.transform.position;
+        }
+
+    }
+
+    private void NoticePlayerUpdate()
+    {
+        RaycastHit hit;
+        _isPlayerNoticed = false;
+        var direction = Player.transform.position - transform.position;
+        if (Vector3.Angle(transform.forward, direction) < ViewAngle)
+        {
+            if (Physics.Raycast(transform.position + Vector3.up, direction, out hit))
+            {
+                if (hit.collider.gameObject == Player.gameObject)
+                {
+                    _isPlayerNoticed = true;
+                }
+            }
+        }
+    }
+
+    private void PickNewPatrolPoint()
+    {
+        _navMeshAgent.destination = PatrolPoints[Random.Range(0, PatrolPoints.Count)].position;
+    }
+
+}
