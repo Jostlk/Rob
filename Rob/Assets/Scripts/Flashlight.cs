6e6b9ea9e6e827baf3f73a75bcff6b3b0775ca72
Jostlk <evgenijlopyrev29@gmail.com>
2024-04-16 20:34:53 +0300

    Flashlight
    Добавил фонарик

---
+++ Rob/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Flashlight : MonoBehaviour
+{
+    public Light _Flashlight;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            _Flashlight.enabled = !_Flashlight.enabled;
+        }
+    }
+}
---
+++ Rob/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5.0f; // Скорость движения персонажа
+    public float jumpForce;
+    public float gravity = 9.8f;
+
+    private CharacterController controller;
+
+    private float _fallVelocity = 0;
+
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        // Получаем ввод от игрока
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Вычисляем направление движения
+        Vector3 moveDirection = transform.forward * verticalInput + transform.right * horizontalInput;
+
+        // Применяем гравитацию
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Двигаем персонажа
+        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
+        {
+            _fallVelocity = -jumpForce;
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            controller.height = 1f;
+        }
+        else
+        {
+            controller.height = 2f;
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (controller.height == 2)
+            {
+                moveSpeed = 10f;
+            }
+        }
+        else
+        {
+            moveSpeed = 5f;
+        }
+    }
+    void FixedUpdate()
+    {
+        _fallVelocity += gravity * Time.fixedDeltaTime;
+        controller.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+        if (controller.isGrounded)
+        {
+            _fallVelocity = 0;
+        }
+    }
+}
