685a08561e49355d6846bd36d5f928c88dc3d337
grapes0 <128805851+grapes0@users.noreply.github.com>
2024-04-22 00:21:06 +0300

    Main Menu
    -Добавил главное меню
    -Заменил GameScreen.cs на InterfaceManager.cs

---
+++ Rob/Assets/Scripts/InterfaceManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InterfaceManager : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit(0);
+    }
+    public void SwitchScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+        Debug.Log($"Loaded scene - {sceneName}");
+    }
+}
