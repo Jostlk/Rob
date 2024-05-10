c6feac4250134578b2dba45f83c1eb3a2214ea3f
grapes0 <128805851+grapes0@users.noreply.github.com>
2024-05-11 00:02:17 +0300

    Simple Pause
    -Добавил паузу на клавишу Esc
    
    Недочеты: не останавливает звук (если кто-то дополнит скрипт чтобы это сделать, то буду благодарен)

---
+++ Rob/Assets/Scripts/UI/Pause.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject gameOverMenu;
+    [SerializeField] CameraController cameraController;
+
+    private float startSens;
+
+    void Start()
+    {
+        startSens = cameraController.sensitivity;
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        cameraController.sensitivity = 0;
+    }
+    public void UnpauseGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        cameraController.sensitivity = startSens;
+    }
+    void Update()
+    {
+        if (!gameOverMenu.activeInHierarchy)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (pauseMenu.activeInHierarchy)
+                {
+                    UnpauseGame();
+                }
+                else
+                { 
+                    PauseGame();
+                }
+            }
+        }
+    }
+}
