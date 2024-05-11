ebd2afeccd0527fc8db9032122d081dc06b58e2d
Jostlk <evgenijlopyrev29@gmail.com>
2024-05-11 17:20:26 +0300

    Add Bombs
    Добавил бомбы

---
+++ Rob/Assets/Scripts/Objects/Bomb/Bomb.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bomb : MonoBehaviour
+{
+    public Image Button;
+    public GameObject BombClear;
+    public GameObject BombObject;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.name == "Capsule")
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                Button.fillAmount += 0.4f * Time.deltaTime;
+                if (Button.fillAmount == 1)
+                {
+                    Destroy(BombClear);
+                    BombObject.SetActive(true);
+                    Destroy(gameObject);
+                }
+            }
+            else
+            {
+                Button.fillAmount -= 2 * Time.deltaTime;
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Button.fillAmount = 0;
+    }
+}
